422c1b81a0c350d883558a57bdd90e4fdfe2f8a5
Harindu <IT21358616@my.sliit.lk>
2023-12-17 22:23:32 +0530

    init

---
+++ controller/UserController.js
@@ -0,0 +1,13 @@
+const userSchema = require('../model/UserSchema');
+const bcrypt= require('bcrypt');
+
+const register = (req,resp) => {
+
+}
+const login = (req,resp) => {
+
+}
+
+module.exports = {
+    register,login
+}
